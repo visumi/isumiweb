bb01d13a49f786c6303d10466485370bbea79e72
Isumi <v206759@dac.unicamp.br>
2021-01-15 21:30:22 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import images from './images';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={images.logo} alt="logo-bg" className="absolute w-80 pt-10 pl-10"></img>
+        <img src={images.header} alt="header-bg" className="static"></img>
       </header>
     </div>
   );
---
+++ src/images.tsx
@@ -0,0 +1,9 @@
+import header from './assets/header.png';
+import logo from './assets/logo.png';
+
+const images = {
+    header,
+    logo
+};
+
+export default images;
