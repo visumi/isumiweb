874af1e0272ab9b4d9fd823d0afb7e1024c892ad
Isumi <v206759@dac.unicamp.br>
2021-01-17 15:50:31 -0300

    removido node-sass e criado botoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import './App.scss';
 import images from './images';
 
 function App() {
   return (
-    <div className="App h-screen">
-      <header className="App-header">
-        <img src={images.logo} alt="logo-bg" className="absolute w-80 pt-10 pl-10"></img>
-        <img src={images.header} alt="header-bg" className="static"></img>
-      </header>
+    <div className="min-h-screen font-sans antialiased text-black bg-blue-100">
+      <div 
+        className="bg-cover bg-no-repeat h-96 p-10 flex justify-between"
+        style={
+          {
+            backgroundImage: `url(${images.header})`
+          }
+        }
+      >
+        <div className="pr-8">
+          <img src={images.logo} alt="Logo do Site"></img>
+        </div>
+        <div className="text-white font-serif text-2xl font-bold flex flex-grow justify-evenly max-w-6xl">
+          <a href="/">Tinga</a>
+          <a href="/">Tiringa</a>
+          <a href="/">Tiringa Tinga</a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/images.tsx
+++ src/images.tsx
@@ -1,5 +1,5 @@
 import header from './assets/header.png';
-import logo from './assets/logo.png';
+import logo from './assets/logo-normal.png';
 
 const images = {
     header,
