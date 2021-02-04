32486e8d6d75f7635ae1eb707d2ae6b769574a87
Isumi <v206759@dac.unicamp.br>
2021-02-04 19:26:51 -0300

    Adicionado texto na home

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import Header from './Header';
 import images from './images';
 
 const App = () => (
-  <div className="font-sans antialiased">
+  <div className="font-sans antialiased bg-primary">
     <Header />
-    <img src={images.aboutBg} alt="Imagem de fundo" />
+    <div className="flex flex-grow justify-between">
+      <div className="mx-48 my-56">
+        <p className="font-serif font-black text-black antialiased text-6xl">Vinícius Isumi</p>
+        <p className="font-serif font-bold text-gray-800 antialiased text-3xl my-2">Desenvolvedor Web</p>
+        <p className="font-sans font-semibold text-gray-600 antialiased text-2xl">
+          Formado em Análise de Sistemas pela UNICAMP, atualmente cursando Mestrado e trabalhando com Angular.
+        </p>
+      </div>
+      <img className="my-12 mx-24" src={images.aboutBg} alt="Imagem de fundo" />
+    </div>
   </div>
 );
 
