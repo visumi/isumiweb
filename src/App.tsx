5a6aa6a516ac40615e1110e56bfbd70757c0bc91
Isumi <v206759@dac.unicamp.br>
2021-02-05 14:12:53 -0300

    Ajustado margins e paddings da home

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,13 @@ const App = () => (
       <div className="mx-48 my-56">
         <p className="font-serif font-black text-black antialiased text-6xl">Vinícius Isumi</p>
         <p className="font-serif font-bold text-gray-800 antialiased text-3xl my-2">Desenvolvedor Web</p>
-        <p className="font-sans font-semibold text-gray-600 antialiased text-2xl">
+        <p className="font-sans font-semibold text-gray-600 antialiased text-2xl max-w-md my-2">
           Formado em Análise de Sistemas pela UNICAMP, atualmente cursando Mestrado e trabalhando com Angular.
         </p>
       </div>
-      <img className="my-12 mx-24" src={images.aboutBg} alt="Imagem de fundo" />
+      <div className="flex flex-grow mt-12 justify-center">
+        <img className="w-8/12" src={images.aboutBg} alt="Imagem de fundo" />
+      </div>
     </div>
   </div>
 );
--- src/header.tsx
+++ src/header.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react';
 
 const Header: FC = () => (
-  <div className="flex md:flex-row flex-col items-center py-4 px-48">
+  <div className="flex md:flex-row flex-col items-center py-10 px-48">
     <div className="flex-grow">
       <h1 className="font-serif font-black text-4xl md:pb-0 pb-4">isumi</h1>
     </div>
-    <div className="sm:block flex flex-col text-center px-4">
-      <a className="font-display text-2xl px-12 sm:py-0 py-1" href="google.com">Sobre</a>
-      <a className="font-display text-2xl px-12 sm:py-0 py-1" href="gmail.com">Projetos</a>
-      <a className="font-display text-2xl px-12 sm:py-0 py-1" href="youtube.com">Contato</a>
+    <div className="sm:block flex flex-col text-center">
+      <a className="font-display text-2xl mx-16 sm:py-0 py-1 px-4" href="google.com">Sobre</a>
+      <a className="font-display text-2xl mx-16 sm:py-0 py-1 px-4" href="gmail.com">Projetos</a>
+      <a className="font-display text-2xl mx-16 sm:py-0 py-1 px-4" href="youtube.com">Contato</a>
     </div>
   </div>
 );
