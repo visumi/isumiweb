7513947830dd4012ae51c06df10c12387a215f27
Isumi <v206759@dac.unicamp.br>
2021-03-28 22:38:46 -0300

    arrumado tamanho da tela

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from './Home';
 import Header from './Header';
 
 const App = () => (
-  <div className="font-sans antialiased bg-primary">
+  <div className="font-sans antialiased bg-primary min-h-screen overflow-y-hidden">
     <Header />
     <Home />
   </div>
--- src/header.tsx
+++ src/header.tsx
@@ -1,48 +1,46 @@
-import React, { FC, useState } from 'react';
-import DarkModeToggle from 'react-dark-mode-toggle';
+import React, { FC } from 'react';
+// import DarkModeToggle from 'react-dark-mode-toggle';
 
-const Header: FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  return (
-    <div className="flex lg:flex-row flex-col items-center py-10 pl-48 select-none">
-      <div className="flex-grow">
-        <h1 className="font-serif font-black text-4xl md:pb-0 pb-4">isumi</h1>
-      </div>
-      <div className="sm:block flex flex-col text-center -mx-8">
-        <a
-          className="inline-block font-display text-2xl mx-16 sm:py-0 py-1 px-4
+const Header: FC = () => (
+  // const [isDarkMode, setIsDarkMode] = useState(false);
+  <div className="flex lg:flex-row flex-col items-center py-10 pl-48 select-none">
+    <div className="flex-grow">
+      <h1 className="font-serif font-black text-4xl md:pb-0 pb-4 hover:text-gray-700">isumi</h1>
+    </div>
+    <div className="sm:block flex flex-col text-center mx-40">
+      <a
+        className="inline-block font-display text-2xl mx-16 sm:py-0 py-1 px-4
         hover:transition duration-150 ease-in-out
         transform hover:scale-125"
-          href="/"
-        >
-          Sobre
-        </a>
-        <a
-          className="inline-block font-display text-2xl mx-16 sm:py-0 py-1 px-4
+        href="/"
+      >
+        Sobre
+      </a>
+      <a
+        className="inline-block font-display text-2xl mx-16 sm:py-0 py-1 px-4
         hover:transition duration-150 ease-in-out
         transform hover:scale-125"
-          href="/"
-        >
-          Projetos
-        </a>
-        <a
-          className="inline-block font-display text-2xl mx-16 sm:py-0 py-1 px-4
+        href="/"
+      >
+        Projetos
+      </a>
+      <a
+        className="inline-block font-display text-2xl mx-16 sm:py-0 py-1 px-4
         hover:transition duration-150 ease-in-out motion-safe
         transform hover:scale-125"
-          href="/"
-        >
-          Contato
-        </a>
-      </div>
-      <div className="flex px-20">
+        href="/"
+      >
+        Contato
+      </a>
+    </div>
+    {/* <div className="flex px-20">
         <DarkModeToggle
           className="focus:outline-none"
           onChange={setIsDarkMode}
           checked={isDarkMode}
           size={60}
         />
-      </div>
-    </div>
-  );
-};
+      </div> */}
+  </div>
+);
 export default Header;
