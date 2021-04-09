0e75c9d736a835b90e7f21fd8d386bbe5dffd23f
Isumi <v206759@dac.unicamp.br>
2021-04-09 12:42:02 -0300

    Tela de sobre componentizada

--- src/About.tsx
+++ src/About.tsx
@@ -1,81 +1,66 @@
 import React, { FC } from 'react';
 import { FiBook, FiBriefcase, FiCode, FiUser } from 'react-icons/fi';
+import Card from './components/Card';
+
+const geralData = [
+  {
+    subtitle: 'Localização',
+    content: 'Atualmente estou morando em Campinas-SP',
+  },
+  {
+    subtitle: 'Habilidades',
+    content: 'Trabalho em grupo, vontade em liderar, dinâmico, artístico',
+  },
+  {
+    subtitle: 'Hobby',
+    content: 'Jogos de computador, gastronomia, cinema, geografia e aprender sobre novos temas',
+  },
+];
+
+const formacaoData = [
+  {
+    subtitle: 'Universidade Estadual de Campinas',
+    content: 'Análise e Desenvolvimento de Sistemas',
+    time: '2017 - 2021',
+  },
+  {
+    subtitle: 'FEEC - UNICAMP',
+    content: 'Mestrado Incompleto na área de IoT',
+    time: '2021',
+  },
+];
+
+const carreiraData = [
+  {
+    subtitle: 'LaRCom FEEC UNICAMP - Estágio',
+    content: 'Líder Técnico de um projeto envolvendo Netsuite da Oracle',
+    time: '2020 - 2021',
+  },
+  {
+    subtitle: 'Enforce BTG Pactual - Estágio',
+    content: 'Trabalho com desenvolvimento de Sistemas Web utilizando Angular',
+    time: '2020 - Hoje',
+  },
+];
+
+const expData = [
+  {
+    subtitle: 'Linguagens',
+    content: 'HTML, CSS, Javascript, Typescript, Node.js, React, Angular, Netsuite',
+  },
+  {
+    subtitle: 'Idiomas',
+    content: 'Inglês Avançado',
+  },
+];
 
 const About: FC = () => (
   <div>
     <div className="my-40 flex justify-center space-x-12">
-      <div>
-        <FiUser className="text-6xl text-black mx-auto" />
-        <p className="font-serif font-bold text-black text-3xl mt-2 mb-6 text-center select-none">Geral</p>
-        <p className="font-sans font-semibold text-black text-xl text-center mt-6">Localização</p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          Atualmente estou morando em Campinas-SP
-        </p>
-        <p className="font-sans font-semibold text-black text-xl text-center mt-6">Habilidades</p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          Trabalho em grupo, vontade em liderar, dinâmico, artístico
-        </p>
-        <p className="font-sans font-semibold text-black text-xl text-center mt-6">Hobby</p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          Jogos de computador, gastronomia, cinema, geografia e aprender sobre novos temas
-        </p>
-      </div>
-      <div>
-        <FiBook className="text-6xl text-black mx-auto" />
-        <p className="font-serif font-bold text-black text-3xl mt-2 mb-6 text-center select-none">Formação</p>
-        <p className="font-sans font-semibold text-black text-xl text-center">
-          2017 - 2021
-        </p>
-        <p className="font-sans font-semibold text-black text-xl max-w-sm text-center">
-          Universidade Estadual de Campinas
-        </p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          Análise e Desenvolvimento de Sistemas
-        </p>
-        <p className="font-sans font-semibold text-black text-xl text-center mt-6">
-          2021 - Hoje
-        </p>
-        <p className="font-sans font-semibold text-black text-xl max-w-sm text-center">
-          Universidade Estadual de Campinas
-        </p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          Mestrado pela FEEC
-        </p>
-      </div>
-      <div>
-        <FiBriefcase className="text-6xl text-black mx-auto" />
-        <p className="font-serif font-bold text-black text-3xl mt-2 mb-6 text-center select-none">Carreira</p>
-        <p className="font-sans font-semibold text-black text-xl text-center">
-          2020-2021
-        </p>
-        <p className="font-sans font-semibold text-black text-xl max-w-sm text-center">
-          LaRCom FEEC UNICAMP - Estágio
-        </p>
-        <p className="font-sans font-semibold text-gray-500 text-lg max-w-sm text-center">
-          Líder Técnico de um projeto envolvendo Netsuite da Oracle
-        </p>
-        <p className="font-sans font-semibold text-black text-xl text-center mt-6">
-          2020-Hoje
-        </p>
-        <p className="font-sans font-semibold text-black text-xl max-w-sm text-center">
-          Enforce BTG Pactual - Estágio
-        </p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          Trabalho com desenvolvimento de Sistemas Web utilizando Angular
-        </p>
-      </div>
-      <div>
-        <FiCode className="text-6xl text-black mx-auto" />
-        <p className="font-serif font-bold text-black text-3xl mt-2 mb-6 text-center select-none">Experiências</p>
-        <p className="font-sans font-semibold text-black text-xl max-w-sm text-center">Linguagens</p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          HTML, CSS, Javascript, Typescript, Node.js, React, Angular, Netsuite
-        </p>
-        <p className="font-sans font-semibold text-black text-xl text-center mt-6">Idiomas</p>
-        <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
-          Inglês Avançado
-        </p>
-      </div>
+      <Card title="Geral" icon={FiUser} data={geralData} />
+      <Card title="Formação" icon={FiBook} data={formacaoData} />
+      <Card title="Carreira" icon={FiBriefcase} data={carreiraData} />
+      <Card title="Experiências" icon={FiCode} data={expData} />
     </div>
   </div>
 );
---
+++ src/components/Card.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import { IconType } from 'react-icons';
+
+interface CardProps {
+  title: string;
+  icon: IconType;
+  data: {
+    subtitle: string;
+    content: string;
+    time?: string;
+  }[];
+}
+
+const Card: FC<CardProps> = ({ title, icon, data }: CardProps) => {
+  const Icon = icon;
+
+  return (
+    <div>
+      <Icon className="text-6xl text-black mx-auto" />
+      <p className="font-serif font-bold text-black text-3xl mt-2 mb-6 text-center select-none">{title}</p>
+      {
+        data.map((d) => (
+          <div key={d.subtitle} className="mt-6">
+            <p className="font-sans font-semibold text-black text-xl text-center">
+              {d.time}
+            </p>
+            <p className="font-sans font-semibold text-black text-xl text-center">{d.subtitle}</p>
+            <p className="font-sans font-semibold text-gray-500 text-xl max-w-sm text-center">
+              {d.content}
+            </p>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
+Card.defaultProps = {};
+
+export default Card;
