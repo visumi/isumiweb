1d18b7e203d6b427b57c1ed285ed4635fea65d87
Isumi <v206759@dac.unicamp.br>
2021-04-19 13:04:43 -0300

    arrumado viteconfig

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -2,11 +2,11 @@ import React, { FC } from 'react';
 import images from './utils/images';
 
 const NotFound: FC = () => (
-  <div className="md:flex md:flex-grow md:justify-between">
-    <div className="md:mx-48 md:my-72 text-center px-4 py-12">
-      <p className="font-serif font-black text-black lg:text-6xl text-4xl">Ops! Página não encontrada.</p>
+  <div className="flex flex-grow justify-between">
+    <div className="mx-48 my-72">
+      <p className="font-serif font-black text-black text-6xl">Ops! Página não encontrada.</p>
     </div>
-    <img className="md:w-1/3 md:mr-40 px-10" src={images.whale} alt="Imagem de uma Baleia" />
+    <img className="w-1/3 mr-40" src={images.whale} alt="Imagem de uma Baleia" />
   </div>
 );
 
