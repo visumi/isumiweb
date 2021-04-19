0e00f770f3b41534eb3c0a66c48aa0254c31454f
Isumi <v206759@dac.unicamp.br>
2021-04-19 13:16:24 -0300

    alterado para http novamente

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [reactRefresh()],
   base: 'https://isumi.com.br/'
+
 })
