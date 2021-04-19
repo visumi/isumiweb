5e7c7388d225792f8732c123603ba0a1a33d8dfa
Isumi <v206759@dac.unicamp.br>
2021-04-19 13:13:20 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [reactRefresh()],
   base: 'https://isumi.com.br/'
 
+=======
+  plugins: [reactRefresh()]
+>>>>>>> parent of e6ff8c9 (atualizado pagina 404 mobile)
 })
