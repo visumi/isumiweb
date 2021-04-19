5f1764814cd43bef43a5d0b7a002355eb39d60e8
Isumi <v206759@dac.unicamp.br>
2021-04-19 00:03:16 -0300

    build teste

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Barbershop from './projects/barbershop';
 
 const App = () => (
   <Router>
-    <div className="font-sans antialiased bg-gradient-to-t from-gray-50 to-gray-100 min-h-screen overflow-y-hidden">
+    <div className="font-sans antialiased bg-gradient-to-t from-gray-50 to-gray-100 min-h-screen overflow-hidden">
       <Header />
       <Switch>
         <Route path="/sobre">
